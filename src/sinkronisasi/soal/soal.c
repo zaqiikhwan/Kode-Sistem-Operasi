0c1636500f1b0e09a1287c9607c7c5744c85edd4
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-04-13 13:47:10 +0700

    ```refactor:``` soal.c

---
+++ src/sinkronisasi/soal/soal.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int counter = 0;
+pthread_t tid[4];
+
+void *doSomeThing(void *arg) { // modifikasi pada fungsi *doSomeThing untuk langkah 2 Tugas 1
+    counter++;
+    printf("\n Thread %d started, please wait...\n", counter);
+    unsigned long i = 0;
+    for(i=0; i<(0xFFFFFFFF);i++);
+    
+    printf("\n Thread %d finished\n", counter);
+    
+    return NULL;
+}
+
+int main(void) {
+    int i;
+    for(i = 0; i < 4; i++)
+        pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
+    for(i = 0; i < 4; i++)
+        pthread_join(tid[i], NULL);
+
+    return 0;
+}
